7ac4f4edf36fcd0ac407c4769652f90c1ad1d67c
Emil Capraru <Emil.Capraru-3rdpty@brooksautomation.com>
2025-06-10 10:40:15 +0300

    commit 10/06

--- obj/Debug/netcoreapp3.1/WebAPI.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0fd8889ddfbafca74a4992498cfa5ec0512b8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f660b7b71a233432a322b2319b955c1817c9baa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
